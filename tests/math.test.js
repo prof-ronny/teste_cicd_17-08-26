d53e0fc366de93141741d671e9242120c3893a5c
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2026-08-18 01:00:34 +0000

    correção da função

--- tests/math.test.js
+++ tests/math.test.js
@@ -5,6 +5,6 @@ test('soma 2 + 3 igual a 5', () => {
 expect(math.add(2, 3)).toBe(5); 
 }); 
 
-test('subtracao 2 - 3 igual a -5', () => { 
-    expect(math.subtract(2, 3)).toBe(-5); 
+test('subtracao 2 - 3 igual a -1', () => { 
+    expect(math.subtract(2, 3)).toBe(-1); 
 }); 
